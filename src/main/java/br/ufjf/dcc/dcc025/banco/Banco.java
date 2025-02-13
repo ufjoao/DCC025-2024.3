1faa6fd3962982ccadd898ccc1b693baa2aecfed
ufjoao <joao.fonseca@estudante.ufjf.br>
2025-02-12 21:23:25 -0300

    Inicializando o Repositório

---
+++ src/main/java/br/ufjf/dcc/dcc025/banco/Banco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.ufjf.dcc.dcc025.banco;
+
+/**
+ *
+ * @author joao
+ */
+public class Banco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
