265ca4c58d7bba9de35e908df99e513881813233
ufjoao <joao.fonseca@estudante.ufjf.br>
2025-02-12 22:09:38 -0300

    Inicializando o Repositório

--- src/main/java/br/ufjf/dcc/dcc025/banco/Banco.java
+++ src/main/java/br/ufjf/dcc/dcc025/banco/Banco.java
@@ -11,6 +11,6 @@ package br.ufjf.dcc.dcc025.banco;
 public class Banco {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!!!");
     }
 }
