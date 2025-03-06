247fd316e1a72f9f2d8d0df436bf3fae9b74f4ed
ufjoao <joao.fonseca@estudante.ufjf.br>
2025-03-06 13:42:45 -0300

    Resolvendo conflitos na classe Cliente

--- src/main/java/banco/model/Cliente.java
+++ src/main/java/banco/model/Cliente.java
@@ -5,19 +5,14 @@ import com.google.gson.annotations.Expose;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Usuario 
-{
+public class Cliente extends Usuario {
 
-    private String nome;
-    private String cpf;
-    private int senha;
-    private ArrayList<Conta> contas = new ArrayList<>(); // Um cliente pode ter várias contas
+    @Expose
+    private List<Conta> contas;
+    @Expose
+    private float limiteCredito = 0f;
 
-    @JsonCreator
-    public Cliente(@JsonProperty("nome") String nome,
-                   @JsonProperty("cpf") String cpf,
-                   @JsonProperty("senha") int senha) 
-    {
+    public Cliente(String nome, String cpf, int senha) {
         super(nome, cpf, senha);
         this.contas = new ArrayList<>();
     }
@@ -34,11 +29,25 @@ public class Cliente extends Usuario
     public List<Conta> getContas() {
         return contas;
     }
-
-    public void adicionarConta(Conta conta) 
+    
+        public Conta retornaConta(int numeroDaConta)
     {
-        if (conta == null) 
+        for(int i=0; i<contas.size(); i++)
         {
+            if(verificaNumeroDaConta(numeroDaConta))
+                return contas.get(i);
+        }
+        
+        System.out.println("Conta não encontrada");
+        return null;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
+
+    public void adicionarConta(Conta conta) {
+        if (conta == null) {
             throw new IllegalArgumentException("Conta não pode ser nula.");
         }
         contas.add(conta);
@@ -85,14 +94,12 @@ public class Cliente extends Usuario
     public void registraMovimentacao(String movimentacao) {
         contas.get(contas.indexOf(this)).adicionarMovimentacao(movimentacao);
     }
-    
-    public float getLimiteCredito() 
-    {
+
+    public float getLimiteCredito() {
         return limiteCredito;
     }
-    
-    public void adicionarLimiteCredito(float valor) 
-    {
+
+    public void adicionarLimiteCredito(float valor) {
         this.limiteCredito += valor;
     }
 }
